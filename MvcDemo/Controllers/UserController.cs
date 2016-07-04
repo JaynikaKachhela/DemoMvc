e2fc8fd0611d2869c0ac8c8ab63a2dae4091dfd2
JaynikaKachhela <jaynika@promactinfo.com>
2016-07-04 17:07:53 +0530

    First time Application is commited.

---
+++ MvcDemo/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public string Index()
+        {
+            return "Index page...";
+        }
+        public string Welcome()
+        {
+            return "welcome to JP...";
+        }
+        public string Count(int count,string name)
+        {
+            return HttpUtility.HtmlEncode("The number given is "+count+". Name of User : "+name);
+        }
+    }
+}
