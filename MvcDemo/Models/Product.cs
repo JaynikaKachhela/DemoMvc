9eede5719a39990c882a4cd49672a2d8b560b680
JaynikaKachhela <jaynika@promactinfo.com>
2016-07-05 19:00:37 +0530

    commited after database

---
+++ MvcDemo/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcDemo.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float price { get; set; }
+    }
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+    }
+}
